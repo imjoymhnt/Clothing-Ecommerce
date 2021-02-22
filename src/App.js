d0ea20fbbe4c9bdacbfb1bd937013d810cd3b4b3
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-02-22 22:07:43 +0530

    add basic router functionality using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => <h1>Hats page</h1>;
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route path="/hats" component={HatsPage} />
+        <Route path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
