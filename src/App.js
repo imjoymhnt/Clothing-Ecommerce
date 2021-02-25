1c159194b0369c88ee8713f44519953b4355be7b
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-02-25 16:11:25 +0530

    add colection preview, collection item component and shop page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
-
-const HatsPage = () => <h1>Hats page</h1>;
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path="/hats" component={HatsPage} />
-        <Route path="/" component={HomePage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,9 +3,11 @@ import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
-  <div className={`${size} menu-item`}>
+  <div
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+    className={`${size} menu-item`}
+  >
     <div
-      onClick={() => history.push(`${match.url}${linkUrl}`)}
       className="background-image"
       style={{ backgroundImage: `url(${imageUrl})` }}
     />
