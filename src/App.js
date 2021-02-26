fda0e81846fe527d5ed008b0de240ac11f1cdc74
Mritunjoy Mahanta <mritunjoy.mahanta001@gmail.com>
2021-02-26 14:02:13 +0530

    add sign in form using signin page and signin component and custom button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/signin-and-signup/signin-and-signup.component";
 import Header from "./components/header/header.component";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
